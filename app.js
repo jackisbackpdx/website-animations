ca1003224829a81d630b746067965e04b551285e
Jackson <jackson.kelley.g@gmail.com>
2020-02-26 19:05:45 -0800

    The random moving diagonal line is working and the picture moving up from bottom on scroll has been added

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+setTimeout(function() {
+    window.scrollTo(0, 0);
+}, 200);
+
 let top = document.getElementById('diagonal-1');
 let middle = document.getElementById('diagonal-2');
 let bottom = document.getElementById('diagonal-3');
@@ -11,12 +15,14 @@ bottom.style.top = '300px';
 function stripeAnimation() {
     for (let i = 0; i < lines.length; i++) {
         let stringToNumber = parseInt(lines[i].style.top);
-        if (stringToNumber <= 950) {
-            stringToNumber += 4;
+        if (stringToNumber <= 1150) {
+            stringToNumber += 6;
             lines[i].style.top = stringToNumber + 'px';
         } 
         else {
-            lines[i].style.top = '-200px';
+            lines[i].style.opacity = '0.0';
+            lines[i].style.top = '-300px';
+            lines[i].style.opacity = '1.0';
         }
     }
     requestAnimationFrame(stripeAnimation);
@@ -30,8 +36,8 @@ quirk.style.top = '0px';
 
 const possibleIntervals = [
     5,
-    1,
     10,
+    50
 ];
 const possibleRanges = [
     50,
@@ -44,7 +50,14 @@ const possibleRanges = [
     400,
     450,
     500,
-    550,
+    550
+];
+
+const orangesAndReds = [
+    'rgb(255, 153, 102)',
+    'rgb(233, 140, 105)',
+    'rgb(226, 144, 91)',
+    'rgb(183, 65, 14)'
 ];
 
 let randomSpeed;
@@ -54,9 +67,10 @@ function quirkAnimation() {
     let stringToNumber = parseInt(quirk.style.top);
     
     if (stringToNumber === randomRange) {
-        let randomNumber1;
-        randomNumber1 = Math.floor(Math.random() * 12);
-        let randomNumber2 = Math.floor(Math.random() * 5);
+        let randomNumber1 = Math.floor(Math.random() * 11);
+        let randomNumber2 = Math.floor(Math.random() * 2);
+        let colorNumber = Math.floor(Math.random() * 4);
+        quirk.style.backgroundColor = orangesAndReds[colorNumber];
         randomRange = possibleRanges[randomNumber1];
         randomSpeed = possibleIntervals[randomNumber2];
     }
@@ -74,14 +88,40 @@ function quirkAnimation() {
     }
     if (stringToNumber < randomRange) {
         stringToNumber += randomSpeed;
-        quirk.style.top = stringToNumber + 'px';
     }
     if (stringToNumber > randomRange) {
         stringToNumber -= randomSpeed;
-        quirk.style.top = stringToNumber + 'px';
     }
-    requestAnimationFrame(quirkAnimation);
-    
+    quirk.style.top = stringToNumber + 'px';
+    requestAnimationFrame(quirkAnimation);  
 }
-
 quirkAnimation();
+
+let previousScrollPosition = 0;
+let currentScrollPosition = 0;
+
+let decreaseBy;
+
+let louvre = document.getElementById('louvre');
+louvre.style.top = '1300px';
+louvre.style.display = 'none';
+
+window.addEventListener('scroll', function() {
+    if (currentScrollPosition > previousScrollPosition) {
+        previousScrollPosition = currentScrollPosition;
+    }
+    currentScrollPosition = window.scrollY;
+    decreaseBy = currentScrollPosition - previousScrollPosition;
+
+    if (currentScrollPosition > 600 && decreaseBy > 0 && louvre.style.top < '600px') {
+        let numberToString = parseInt(louvre.style.top);
+
+        numberToString -= decreaseBy;
+        louvre.style.top = numberToString + 'px';
+        louvre.style.display = 'flex';
+    } else {
+        louvre.style.top = '1300px';
+    }
+    console.log(louvre.style.top);
+    console.log(currentScrollPosition);
+});
